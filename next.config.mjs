a25539ed6e0382ef9011bf729cc457257476d7cc
rafioosadani <rafioosadani@gmail.com>
2025-07-06 08:05:58 +0700

    refactor: cloudbuild.yaml add .env.prod and modify next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'http', // Atau 'https' jika Laravel Anda sudah menggunakan HTTPS
-                hostname: 'localhost',
+                hostname: '34.101.212.234',
                 port: '8000', // Pastikan ini sesuai dengan port Laravel Anda
                 pathname: '/storage/**', // Path di Laravel tempat gambar disimpan
             },
